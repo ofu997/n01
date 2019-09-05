d1a3dd6bf1238696391b85102884467e1036f4be
ofu997 <ofu997@gmail.com>
2019-09-04 22:29:22 -0700

    add written server

--- index.js
+++ index.js
@@ -3,11 +3,94 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('<h1 style="color: #009000; background-color: #d8ffd8; text-shadow: 2px 2px 4px #b1b1ff; padding: 10px; border: 1px solid #5593c6;">Homepage</h1>')
+    // if (req.url === '/') {
+    //     fs.readFile(
+    //         path.join(__dirname, 'public', 'index.html'), 
+    //         (err, content) => {
+    //             if(err) throw err;
+    //             res.writeHead(200, {'Content-Type': "text/html"})
+    //             res.end(content)
+    //         })
+    // }
+
+    // if (req.url === '/api/users') {
+    //     const users = [
+    //         {name:'Bob Smith', age: 22},
+    //         {name:'Ana Navarro', age: 32},
+    //         {name:'Candice Sharma', age: 42},
+    //     ]
+    //     res.writeHead(20,{"Content-Type":"application/json"})
+    //     res.end(JSON.stringify(users))
+    // }    
+
+    // build filePath
+    let filePath=path.join(
+        __dirname,
+        'public',
+        req.url==='/'?
+        'index.html':req.url
+    );
+
+    // extension of file
+    let extname=path.extname(filePath)
+    // initial content type
+    let contentType ='text/html';
+    // check ext and set content type
+    switch(extname){
+        case '.js':
+            contentType='text/javascript';
+            break;
+        case '.css':
+            contentType='text/css';
+            break;
+        case '.json':
+            contentType='application/json';
+            break;
+        case '.png':
+            contentType='image/png';
+            break;
+        case '.jpg':
+            contentType='image/jpg';
+            break;
     }
+
+    // check if contentType is tet/html but no .html file extension
+    if (contentType == 'text/html' && extname == '') filePath += '.html';
+
+
+
+
+
+    // read file
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == "ENOENT"){
+                // not found
+                fs.readFile(
+                    path.join(__dirname, 'public', '404.html'),
+                    (err, content) => {
+                        res.writeHead(200, {'Content-Type': 'text/html'})
+                        res.end(content, 'utf8')
+                    }
+                )
+            }
+            else {
+                // server error
+                res.writeHead(500)
+                res.end('Server Error ${err.code}')
+            }
+        }
+        // no error
+        else {
+            // success
+            res.writeHead(200, {'Content-Type': 'contentType'})
+            res.end(content, 'utf8')
+        }
+    })
+
 })
 
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
