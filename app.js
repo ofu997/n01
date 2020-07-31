be65886b3e3cf5ef74ccc8ca64c2bb50923921f2
ofu997 <ofu997@gmail.com>
2020-07-30 23:06:09 -0700

    route and redirect requests

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const http = require(`http`);
-const { Http2ServerRequest } = require("http2");
+const fs = require('fs'); 
 
-function rqListener(req, res) {
-
+const server = http.createServer((req,res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url ==='/') {
+  res.write(`<html>`)
+  res.write(`<head><title>A page written in node</title></head>`)
+  res.write(`<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></form></body>`)
+  res.write(`</html>`) 
+  return res.end();
 }
-
-// http.createServer(rqListener);
-
-// http.createServer(function(req, res) {
-
-// });
-
-const server = http.createServer((req,res)=>{
-  console.log(req.url, req.method, req.headers)
-  res.setHeader('Content-Type', 'text/html');
+if (url === '/message' && method === 'POST') {
+  fs.writeFileSync('message.txt', 'dummy')
+  res.statusCode = 302;
+  res.setHeader('Location', '/')
+  return res.end()
+}
+  res.setHeader('Content-type', 'text/html');
   res.write(`<html>`)
   res.write(`<head><title>A page written in node</title></head>`)
   res.write(`<body><h1>Hello from server</h1></body>`)
   res.write(`</html>`) 
-  res.end()
-})
+  res.end();
+});
 
 // prevents exiting script
 server.listen(3000);
