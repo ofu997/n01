c84322ce3496354dd99f56258afaae7bf0bc5277
oliver fu <ofu997@gmail.com>
2020-08-31 00:55:23 -0700

    read requests from form and write to file

--- app.js
+++ app.js
@@ -5,18 +5,30 @@ const server = http.createServer((req,res) => {
   const url = req.url;
   const method = req.method;
   if (url ==='/') {
-  res.write(`<html>`)
-  res.write(`<head><title>A page written in node</title></head>`)
-  res.write(`<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></form></body>`)
-  res.write(`</html>`) 
-  return res.end();
-}
-if (url === '/message' && method === 'POST') {
-  fs.writeFileSync('message.txt', 'dummy')
-  res.statusCode = 302;
-  res.setHeader('Location', '/')
-  return res.end()
-}
+    res.write(`<html>`)
+    res.write(`<head><title>A page written in node</title></head>`)
+    // form sends input data to request body. { message: input.value }
+    res.write(`<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>`)
+    res.write(`</html>`) 
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk); 
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody); 
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end()
+      });
+    });
+  }
   res.setHeader('Content-type', 'text/html');
   res.write(`<html>`)
   res.write(`<head><title>A page written in node</title></head>`)
