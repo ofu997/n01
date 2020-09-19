d63ff23308576c028cd64ce688f60dc8b7562440
oliver fu <ofu997@gmail.com>
2020-09-19 10:56:13 -0700

    explore Express

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware')
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
     next(); // allows req to continue to next middleware
 });
 
-app.use((req, res, next) => {
-    console.log('in the 2nd middleware')
-    res.send('<h1>Hello from Express</h1>'); 
+app.use('/add-product', (req, res, next) => {
+    console.log('in /add-product');
+    res.send('<h1>Add product</h1>'); 
+});
+
+app.use('/', (req, res, next) => {
+  console.log('in \'hello from express\'');
+  res.send('<h1>Hello from Express</h1>'); 
 });
 
 // const server = http.createServer(app);
