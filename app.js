a13b0746cb3577edec840f0ad5c1aa87b12b0e3f
oliver fu <ofu997@gmail.com>
2020-09-19 14:16:14 -0700

    add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
     console.log('This always runs');
@@ -9,8 +12,13 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
     console.log('in /add-product');
-    res.send('<h1>Add product</h1>'); 
+    res.send(`<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</form>`); 
 });
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
   console.log('in \'hello from express\'');
