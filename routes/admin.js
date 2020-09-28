c7f67e9389b0172cc8b28970bf862ea65221b7f7
oliver fu <ofu997@gmail.com>
2020-09-28 13:46:01 -0700

    switch to EJS with partial views

--- routes/admin.js
+++ routes/admin.js
@@ -10,8 +10,14 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    console.log('in /add-product');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  console.log('in /add-product');
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
