f946f379c485eb71f13a082cb67d869a9d972142
oliver fu <ofu997@gmail.com>
2020-10-13 23:11:36 -0700

    MongoDB changes for controllers

--- models/product.js
+++ models/product.js
@@ -1,30 +1,39 @@
-const db = require('../util/database');
+const getDb = require('../util/database').getDb;
 
-const Cart = require('./cart');
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id; 
+class Product {
+  constructor(title, price, description, imageUrl) {
     this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
     this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
 
   save() {
-    return db.execute(
-      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
+    const db =getDb();
+    db.collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  static deleteById(id) {}
-  
   static fetchAll() {
-    return db.execute('SELECT * FROM products');
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(`products are ${products}`);
+        return products; 
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
-  
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+}
+
+module.exports = Product; 
